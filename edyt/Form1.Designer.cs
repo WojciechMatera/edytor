8faf66b0dfbad3c14495267268ba250df7d4fcfa
WojciechMatera <wojciech.matea@gmail.com>
2023-02-23 00:14:32 +0100

    Pierwszy etap

---
+++ edyt/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace edyt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelDropDown = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panelDropDown.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.panelDropDown);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Controls.Add(this.button10);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(591, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(209, 450);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Adjustments";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // panelDropDown
+            // 
+            this.panelDropDown.Controls.Add(this.button9);
+            this.panelDropDown.Controls.Add(this.button8);
+            this.panelDropDown.Controls.Add(this.button7);
+            this.panelDropDown.Controls.Add(this.button6);
+            this.panelDropDown.Controls.Add(this.button5);
+            this.panelDropDown.Location = new System.Drawing.Point(3, 39);
+            this.panelDropDown.MaximumSize = new System.Drawing.Size(200, 193);
+            this.panelDropDown.MinimumSize = new System.Drawing.Size(200, 52);
+            this.panelDropDown.Name = "panelDropDown";
+            this.panelDropDown.Size = new System.Drawing.Size(200, 179);
+            this.panelDropDown.TabIndex = 1;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button9.Location = new System.Drawing.Point(0, 148);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(200, 33);
+            this.button9.TabIndex = 5;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button8.Location = new System.Drawing.Point(0, 115);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(200, 33);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button7.Location = new System.Drawing.Point(0, 82);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(200, 33);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "Font";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button6.Location = new System.Drawing.Point(0, 49);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(200, 33);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "kolor";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Image = global::edyt.Properties.Resources.Expand_Arrow_20px;
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button5.Location = new System.Drawing.Point(0, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(200, 49);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Filters";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(3, 224);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Effects";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(3, 260);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 30);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Resize";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(3, 296);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 49);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Export";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button10.Location = new System.Drawing.Point(3, 351);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(200, 49);
+            this.button10.TabIndex = 4;
+            this.button10.Text = "Upload";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(4, 60);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(578, 337);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "Załaduj tekst";
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(12, 13);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(63, 38);
+            this.button11.TabIndex = 2;
+            this.button11.Text = "Open";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(81, 13);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(63, 38);
+            this.button12.TabIndex = 3;
+            this.button12.Text = "Save";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(164, 13);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(63, 38);
+            this.button13.TabIndex = 4;
+            this.button13.Text = "undo";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(233, 12);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(63, 38);
+            this.button14.TabIndex = 5;
+            this.button14.Text = "redo";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(314, 12);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(63, 38);
+            this.button15.TabIndex = 6;
+            this.button15.Text = "tools";
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(383, 12);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(63, 38);
+            this.button16.TabIndex = 7;
+            this.button16.Text = "exit";
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::edyt.Properties.Resources.lnia;
+            this.pictureBox1.Location = new System.Drawing.Point(148, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 37);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::edyt.Properties.Resources.lnia;
+            this.pictureBox2.Location = new System.Drawing.Point(300, 14);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(10, 36);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(476, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Edyt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Edyt";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panelDropDown.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panelDropDown;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ edyt/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace edyt
+{
+    public partial class Form1 : Form
+    {
+        private bool isCollapsed;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isCollapsed)
+            {
+            //    button5.Image = Resources.Collapse_Arrow_20px;
+                panelDropDown.Height += 10;
+                if (panelDropDown.Size == panelDropDown.MaximumSize)
+                {
+                    timer1.Stop();
+                    isCollapsed = false;
+                }
+            }
+            else
+            {
+            //    button5.Image = Resources.Expand_Arrow_20px;
+                panelDropDown.Height -= 10;
+                if (panelDropDown.Size == panelDropDown.MinimumSize)
+                {
+                    timer1.Stop();
+                    isCollapsed = true;
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        MemoryStream userInput = new MemoryStream();
+        private void button11_Click(object sender, EventArgs e)
+        {
+                // Create an OpenFileDialog to request a file to open.
+                 OpenFileDialog openFile1 = new OpenFileDialog();
+
+                 // Initialize the OpenFileDialog to look for RTF files.
+                 openFile1.DefaultExt = "*.txt";
+                 openFile1.Filter = "TXT Files|*.txt";
+
+            // Determine whether the user selected a file from the OpenFileDialog.
+            if (openFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+               openFile1.FileName.Length > 0)
+            {
+                // Load the contents of the file into the RichTextBox.
+              //  richTextBox1.LoadFile(openFile1.FileName);
+
+
+                // Display the entire contents of the stream,
+                // by setting its position to 0, to RichTextBox2.
+                userInput.Position = 0;
+                
+                richTextBox1.LoadFile(openFile1.FileName, RichTextBoxStreamType.PlainText);
+                Find(richTextBox1, "html", Color.Blue);
+                Find(richTextBox1, "head", Color.Red);
+                Find(richTextBox1, "body", Color.Green);
+                Find(richTextBox1, "name", Color.Silver);
+
+            }
+        }
+        private void AddFontStyle(FontStyle style)
+        {
+            int start = richTextBox1.SelectionStart;
+            int len = richTextBox1.SelectionLength;
+            System.Drawing.Font currentFont;
+            FontStyle fs;
+            for (int i = 0; i < len; ++i)
+            {
+                richTextBox1.Select(start + i, 1);
+                currentFont = richTextBox1.SelectionFont;
+                fs = currentFont.Style;
+                //add style
+                fs = fs | style;
+                richTextBox1.SelectionFont = new Font(
+                  currentFont.FontFamily,
+                  currentFont.Size,
+                  fs
+                  );
+            }
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // Index
+            int index = 0;
+
+            // Loop over each line
+            foreach (string line in richTextBox1.Lines)
+            {
+                // Ignore the non-assembly lines
+                if (line.Substring(0, 2) != ";;")
+                {
+                    // Start position
+                    int start = (richTextBox1.GetFirstCharIndexFromLine(index) + line.LastIndexOf(";") + 1);
+                 
+
+                    // Length
+                    int length = line.Substring(line.LastIndexOf(";"), (line.Length - (line.LastIndexOf(";")))).Length;
+
+                    System.Diagnostics.Debug.WriteLine(start + " " + length);
+                  //  int length = richTextBox1.SelectionLength                   
+                  // Make the selection
+                    richTextBox1.SelectionStart = start;
+                    richTextBox1.SelectionLength = length;
+
+                    // Change the colour
+                    richTextBox1.SelectionColor = Color.Red;
+
+                }
+
+                // Increase index
+                index++;
+            }
+        }
+        public static void Find(RichTextBox rtb, String word, Color color)
+        {
+            if (word == "")
+            {
+                return;
+            }
+            int s_start = rtb.SelectionStart, startIndex = 0, index;
+            System.Diagnostics.Debug.WriteLine(s_start); 
+            while ((index = rtb.Text.IndexOf(word, startIndex)) != -1)
+            {
+                rtb.Select(index, word.Length);
+                rtb.SelectionColor = color;
+                startIndex = index + word.Length;
+            }
+       /*     rtb.SelectionStart = 0;
+            rtb.SelectionLength = rtb.TextLength;
+            rtb.SelectionColor = Color.Black;*/
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            AddFontStyle(FontStyle.Bold);
+        }
+    }
+}
